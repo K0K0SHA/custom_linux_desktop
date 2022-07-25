a2467b4761949345c3ed121cf52da40c7c6dc050
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-07-25 06:15:40 -0400

    Update conky_config.sh
    To everyone who says this looks ugly as f*ck, it's actually supposed to. That's how art works.

--- conky_config.sh
+++ conky_config.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env
-# TODO checkroot
+if [ "$EUID" -ne 0 ]
+  then echo "This installer must be run as root."
+  exit
+fi
+# todo explicit install check
 apt-get -y install conky
-chmod +x ./conky.sh # gives itself executable
-chmod +x ./conky
-cd /etc/conky/ # enter config directory
-echo "gedit /etc/conky/conky.config"
-gedit ./conky.config
+chmod +x ./conky.sh # gives itself executable (not recommended to run this item in BETA)
+chmod +x ./conky    # currently also not recommended
+chmod +x ./conky.py
 
+echo "cloning https://github.com/K0K0SHA/custom_linux_desktop into ."
+git clone https://github.com/K0K0SHA/custom_linux_desktop
+cp ./conky.config /etc/conky/conky.conf
 # TODO customizes Conky using downloaded config file
-# TODO OS compatibility lit
+# TODO OS compatibility list
+# TODO terminal theme and wallpaper functions
